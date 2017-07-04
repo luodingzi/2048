2014fcacdfb21fba26e5a4ff540250e950c4d3ae
luodingzi <luodingzi@gmail.com>
2017-07-04 15:35:39 +0800

    2048

---
+++ js/animate.js
@@ -0,0 +1,37 @@
+
+ 
+function showNumberAnimate(i,j,randomNum){
+	var numberCell=$('#number_cell_'+i+'_'+j+'');
+	numberCell.css('color', getNumberColol(randomNum));
+	numberCell.css('background-color',getBackgroundColor(randomNum));
+	numberCell.text(randomNum);
+
+
+	numberCell.animate({
+		width:cellSideLength,
+		height:cellSideLength,
+		top:getTop(i,j),
+		left:getLeft(i,j)
+	},122)
+
+}
+
+function showMoveAnimate(fromX,fromY,toX,toY){
+	var numberCell=$('#number_cell_'+fromX+'_'+fromY+'');
+	
+
+
+	numberCell.animate({
+		width:cellSideLength,
+		height:cellSideLength,
+		top:getTop(toX,toY),
+		left:getLeft(toX,toY)
+	},122)
+
+}
+
+//更新分数
+function updateScore(score){
+
+	$('#score').html(score+"");
+}
